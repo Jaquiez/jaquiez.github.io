9956bb234e0ec1a945872d910bb008881739bd84
Jaquiez <44443475+Jaquiez@users.noreply.github.com>
2026-02-16 23:07:09 -0500

    styling rework and add old blog

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-/*
-
-Uses innerHTML (sorry) to lazily load "output" from each command.
-
-*/
-
-/*
-
-*/
 const LoadingHelper = {
-    "welcome":`<h2>root@localhost: ~$ ./welcome </h2>`,
-    "whoami":`    
-<p>
-root@localhost: ~$ whoami
-</p>           
-<ul>
-    <li>A CS student @ UMass Amherst with an interest in web + low level security.
-    </li>
-    <li>
-        Learning web application security through CTFs.
-    </li>
-    <li>
-        Learning low level security through... lots of patience.
-    </li>
-    <li>
-        Playing CTF's with <a target="_blank" href="https://ctftime.org/team/78233">SavedByTheShell!</a>
-    </li>
-</ul>`,
-    "ls":`            <p>
+    "welcome": `<h2>root@localhost: ~$ ./welcome </h2>`,
+    "welcome-container": `<div id="whoami"><p>
+                    root@localhost: ~$ whoami
+                </p>
+                <ul>
+                    <li>An Offensive Security engineer with an interest in web + low level security.
+                    </li>
+                    <li>
+                        Learning web application security through CTFs.
+                    </li>
+                    <li>
+                        Learning low level security through... lots of patience.
+                    </li>
+                    <li>
+                        Playing CTF's with <a target="_blank" href="https://ctftime.org/team/367652/">Leg.</a>
+                    </li>
+                </ul></div>
+            <img class="profile-picture" src="mrkrabs.jpg">`,
+    "ls": `            <p>
     root@localhost: ~$ ls
 </p>
 <div style="text-align: center;">
@@ -36,7 +27,7 @@ root@localhost: ~$ whoami
     <a href="/Contact">Contact</a>
     <a href="#readme">README.txt</a>
 </div>`,
-    "readme":`            <p>
+    "readme": `            <p>
     root@localhost: ~$ cat README.txt
 </p>
 <p>
@@ -44,27 +35,41 @@ root@localhost: ~$ whoami
 </p>`
 }
 
-function sleep(ms){
-    return new Promise(async (res,rej)=>{
-        await setTimeout(res,ms)
-    })
+function sleep(ms) {
+    return new Promise((res) => setTimeout(res, ms));
 }
-window.onload = async ()=>{
-    /*
-        Had to write this with normal loops instead of higher order functions. The latter resulted in weird unintended behavior that seems "multithreaded"
-    */
+
+async function typeText(ELEM,content, delay, keepCursor) {
+    return new Promise(async (res) => {
+        for (let i = 0; i < content.length; ++i) {
+            await sleep(delay)
+            ELEM.innerHTML = content.substring(0, i) + `<span class="typing-cursor">&nbsp;</span> `;
+        }
+        if(!keepCursor){
+            ELEM.innerHTML = content;
+        }
+        res(true);
+    })
+
+}
+async function loadIndex() {
     const KEYS = Object.keys(LoadingHelper);
-    for(let x = 0;x<KEYS.length;++x){
+    for (let x = 0; x < KEYS.length; ++x) {
         const key = KEYS[x];
         const ELEM = document.getElementById(key);
-        if(ELEM.nodeName === 'DIV'){
-            await sleep(250);
-            ELEM.classList.replace('transparent','panel');
-        }
-        for(let i=0;i<LoadingHelper[key].length;++i){
-            await sleep(10)
-            ELEM.innerHTML = LoadingHelper[key].substring(0,i);
-        }
-    }  
 
+        // Start with transition class and bubbles visible
+        ELEM.classList.replace('transparent', 'transition');
+        await sleep(100);
+        ELEM.classList.replace('transition', 'panel');
+        await typeText(ELEM,LoadingHelper[key],10,true);
+    }
+
+}
+
+window.onload = () => {
+    document.body.style.setProperty('--bubble-gif-url', `url('/spongebob-bubbles.gif?t=${Date.now()}')`);
+    if (window.location.pathname === '/') {
+        loadIndex();
+    }
 }
